bb3bf1dd571132f96e43c5ee111b02d51f83a714
Carmen Chirica <carmen@globalsoftsys.ro>
2022-02-14 17:29:59 +0200

    Create Pet index page and controller. Closes #22

---
+++ pet-clinic-web/src/main/java/ch/springframework/sfgpetclinic/controllers/PetsController.java
@@ -0,0 +1,14 @@
+package ch.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PetsController {
+
+    @RequestMapping({"/pets", "/pets/index", "/pets/index.html"})
+    public String listPets() {
+
+        return "pets/index";
+    }
+}
