c0cebd2b03d624ef879bb2dee8ea5b891ee6f2f0
Carmen Chirica <carmen@globalsoftsys.ro>
2022-02-15 23:21:01 +0200

    List all Owners on Owners Index page. Closes #24

--- pet-clinic-web/src/main/java/ch/springframework/sfgpetclinic/controllers/PetsController.java
+++ pet-clinic-web/src/main/java/ch/springframework/sfgpetclinic/controllers/PetsController.java
@@ -1,14 +1,22 @@
 package ch.springframework.sfgpetclinic.controllers;
 
+import ch.springframework.sfgpetclinic.services.PetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PetsController {
 
-    @RequestMapping({"/pets", "/pets/index", "/pets/index.html"})
-    public String listPets() {
+    private PetService petService;
 
+    public PetsController(PetService petService) {
+        this.petService = petService;
+    }
+
+    @RequestMapping({"/pets", "/pets/index", "/pets/index.html"})
+    public String listPets(Model model) {
+        model.addAttribute("pets", petService.findAll());
         return "pets/index";
     }
 }
