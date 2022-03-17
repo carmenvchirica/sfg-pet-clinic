c443b3f94b082f900b7eac9547a8f68a684ee7c4
Carmen Chirica <carmen@globalsoftsys.ro>
2022-03-17 12:32:31 +0200

    Refactor and clean.

--- pet-clinic-web/src/test/java/ch/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/ch/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 import static org.hamcrest.Matchers.*;
@@ -44,25 +43,6 @@ class OwnerControllerTest {
         owners.add(Owner.builder().id(2L).build());
 
         mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
-
-    }
-
-    @Test
-    void listOwners() throws Exception {
-        when(ownerService.findAll()).thenReturn(new HashSet<>(owners));
-        mockMvc.perform(get("/owners"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("owners/index"))
-                .andExpect(model().attribute("owners", hasSize(2)));
-    }
-
-    @Test
-    void listOwnersByIndex() throws Exception {
-        when(ownerService.findAll()).thenReturn(new HashSet<>(owners));
-        mockMvc.perform(get("/owners/index"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("owners/index"))
-                .andExpect(model().attribute("owners", hasSize(2)));
     }
 
     @Test
